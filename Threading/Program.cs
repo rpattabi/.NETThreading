666f2dc8671ccc2a6bf0fc62578b8aedbb928d73
rpattabi <rpattabi@users.noreply.github.com>
2014-03-11 13:16:09 +0530

    Added basic use of Thread class

---
+++ Threading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			_1_Basic();
+		}
+
+        static void _1_Basic()
+		{
+			Thread t = new Thread(WriteY);
+			t.Start();
+
+			for (int i = 0; i < 1000; i++)
+			{
+				Console.Write("x");
+			}
+        }
+
+        static void WriteY()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				Console.Write("y");
+			}
+        }
+	}
+}
