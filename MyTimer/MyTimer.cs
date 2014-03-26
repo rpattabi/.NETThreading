de92cc99e98ff38c018649b9ff6a1e41b375696a
rpattabi <rpattabi@users.noreply.github.com>
2014-03-26 07:26:23 +0530

    Attempted to implement timer using threads. Not working as expected yet

---
+++ MyTimer/MyTimer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace MyTimer
+{
+    class MyTimer
+    {
+        private Stopwatch _stopwatch;
+        private Thread _thread;
+
+        public int Interval_ms { get; set; }
+
+        public MyTimer()
+        {
+            this.Interval_ms = 1;
+        }
+
+        public void Start()
+        {
+            Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+
+            _thread = new Thread(() =>
+            {
+                _stopwatch = Stopwatch.StartNew();
+
+                try
+                {
+                    while (_stopwatch.IsRunning)
+                    {
+                        if (_stopwatch.ElapsedMilliseconds == 0)
+                            continue;
+
+                        if (_stopwatch.ElapsedMilliseconds >= this.Interval_ms)
+                            break;
+
+                        if (this.Interval_ms % _stopwatch.ElapsedMilliseconds == 0)
+                            if (Tick != null)
+                                dispatcher.BeginInvoke((Action) (()=>Tick(this, new EventArgs())));
+                    }
+                }
+                finally
+                {
+                    _stopwatch.Stop();
+                }
+            });
+
+            _thread.Start();
+        }
+
+        public void Stop()
+        {
+            _thread.Abort();
+        }
+
+        public event EventHandler Tick;
+    }
+}
---
+++ MyTimer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer_5ms = new MyTimer() { Interval_ms = 5 };
+            timer_5ms.Tick += timer_5ms_Tick;
+            timer_5ms.Start();
+
+            var stopwatch = Stopwatch.StartNew();
+            while (true)
+                if (stopwatch.ElapsedMilliseconds >= 10000)
+                    break;
+                else
+                    Thread.Yield();
+
+            stopwatch.Stop();
+        }
+
+        static void timer_5ms_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("5ms tick");
+        }
+    }
+}
