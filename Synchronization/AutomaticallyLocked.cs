e82e6b74e9571d795ae16f4cf67cb9d8aad1a250
rpattabi <rpattabi@users.noreply.github.com>
2014-05-28 20:14:58 +0530

    Added simple example for automatic locking

---
+++ Synchronization/AutomaticallyLocked.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Contexts;
+using System.Threading;
+
+namespace Synchronization
+{
+    [Synchronization]
+    public class AutomaticallyLocked : ContextBoundObject
+    {
+        public void Demo()
+        {
+            Console.Write("Start...");
+            Thread.Sleep(1000);
+            Console.WriteLine("end");
+        }
+    }
+
+    public class Test
+    {
+        public static void Main()
+        {
+            AutomaticallyLocked safeInstance = new AutomaticallyLocked(); // Returns a proxy! 
+
+            // Call Demo three times concurrently
+            new Thread(safeInstance.Demo).Start();
+            new Thread(safeInstance.Demo).Start();
+            safeInstance.Demo();
+        }
+    }
+}
+
